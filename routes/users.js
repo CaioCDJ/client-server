8b61c266b367599c6c7f3e66b6ae76ffa66e1314
CaioCDJ <caiodjesus1@hotmail.com>
2022-05-10 14:56:09 -0300

    POST, PUT, DELETE with restify

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,43 @@ router.get('/', function(req, res, next) {
   client.get('/users', (err,request,response,obj)=>{
     assert.ifError(err);
 
-    res.end(JSON.stringify(obj,null,2))
+    res.json(obj);
+  });
+});
+// GET user by id
+router.get('/:id', function(req, res, next) {
+
+  client.get(`/users/${req.params.id}`, (err,request,response,obj)=>{
+    assert.ifError(err);
+
+    res.json(obj);
+  });
+});
+// PUT edit user info
+router.put('/:id', function(req, res, next) {
+
+  client.put(`/users/${req.params.id}`,req.body, (err,request,response,obj)=>{
+    assert.ifError(err);
+
+    res.json(obj);
+  });
+});
+// DELETE user
+router.delete('/:id', function(req, res, next) {
+
+  client.del(`/users/${req.params.id}`, (err,request,response,obj)=>{
+    assert.ifError(err);
+
+    res.json(obj);
+  });
+});
+
+router.post('/', function(req, res, next) {
+
+  client.post(`/users/${req.params.id}`, (err,request,response,obj)=>{
+    assert.ifError(err);
+
+    res.json(obj);
   });
 });
 
